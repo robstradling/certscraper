0e7b6d453dbe1c47ceff868240eec0bf9b2b990e
Rob Stradling <rob@comodo.com>
2017-08-25 15:22:09 +0100

    In splitbundle.sh, ensure that there's a \n immediately after -----END CERTIFICATE-----.

--- splitbundle.sh
+++ splitbundle.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 cat "$1" | sed "s/^&gt;//g" | csplit -f "$1-split-" /dev/stdin '/-----BEGIN CERTIFICATE-----/' '{*}'
+find -wholename "$1-split-*" -exec sed -i "s/-----END CERTIFICATE-----/-----END CERTIFICATE-----\n/g" '{}' ';'
